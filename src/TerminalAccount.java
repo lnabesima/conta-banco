ed21b2a21d46a2a1cd78c4160c715009484ead1d
Lucas Nabesima <lucas.nabesima@gmail.com>
2024-08-07 08:13:56 -0300

    feat: handle invalid input for initial balance
    Added a loop to catch InputMismatchException when entering the initial
    balance. This prompts the user to input a decimal value until a valid
    one is entered, improving user input validation.

--- src/TerminalAccount.java
+++ src/TerminalAccount.java
@@ -1,3 +1,5 @@
+import java.util.InputMismatchException;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class TerminalAccount {
@@ -18,8 +20,17 @@ public class TerminalAccount {
         System.out.print("Por favor, insira o nome do cliente: ");
         this.clientName = sc.nextLine();
 
-        System.out.print("Por favor, insira o saldo inicial: ");
-        this.balance = sc.nextDouble();
+        boolean isBalanceInputCorrect = false;
+        do {
+            try {
+                System.out.print("Por favor, insira o saldo inicial: ");
+                this.balance = sc.nextDouble();
+                isBalanceInputCorrect = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Por favor, insira apenas valores decimais.");
+            }
+        } while (!isBalanceInputCorrect);
+
 
 
         System.out.println("Olá " + this.clientName + ", obrigado por criar uma conta em nosso banco. sua agência é " + this.branch + ", conta " + this.account + " e seu saldo " + this.balance + " já está disponível para saque.");
